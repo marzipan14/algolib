989bac28f34de98b9715e529662a8045787d72a6
Mateusz Krajewski <mkrajewski413@gmail.com>
2022-01-29 17:57:38 +0000

    Add tests+javadoc for GraphCompleteTest

--- src/graph/test/GraphCompleteTest.java
+++ src/graph/test/GraphCompleteTest.java
@@ -2,8 +2,19 @@ package graph.test;
 
 import graph.Graph;
 
+/**
+* Makes it possible to check if the graph is complete.
+* Utilises an additional flag "__is_complete".
+*/
 public final class GraphCompleteTest {
+	/**
+	* Performs the check.
+	*
+	* @param g the graph to perform the check on.
+	* @return true if the graph is complete, false otherwise. 
+	*/
 	public <K, V> boolean check(Graph<K, V> g) {
+		if(g == null) return false;
 		g.globalFlags.add("__is_complete", true);
 		g.forEach((key, value) -> {
 			if(g.neighbourSet(key).size() < g.size()-1) {
---
+++ tests/graph/test/GraphCompleteTestTest.java
@@ -0,0 +1,96 @@
+package graph.test;
+
+import static org.junit.Assert.assertTrue; 
+import static org.junit.Assert.assertFalse; 
+import org.junit.Test;
+import org.junit.Before;
+import graph.Graph;
+
+public class GraphCompleteTestTest {
+	private Graph<Integer, String> g;
+	private GraphCompleteTest test;
+
+	@Before
+	public void init() {
+		g = new Graph<Integer, String>();
+		test = new GraphCompleteTest();
+	}
+
+	@Test
+	public void testCompleteWithSingleCompleteGraph() {
+		g.put(1, "a");
+		g.put(2, "b");
+		g.put(3, "c");
+		g.put(4, "d");
+		g.attachBoth(1, 2);
+		g.attachBoth(1, 3);
+		g.attachBoth(1, 4);
+		g.attachBoth(2, 4);
+		g.attachBoth(3, 4);
+		g.attachBoth(2, 3);
+		assertTrue(test.check(g));
+	}
+
+	@Test
+	public void testCompleteWithTwoSeparateCompleteGraphs() {
+		g.put(1, "a");
+		g.put(2, "b");
+		g.put(3, "c");
+		g.put(4, "d");
+		g.put(5, "e");
+		g.put(6, "f");
+		g.put(7, "g");
+		g.attachBoth(1, 2);
+		g.attachBoth(1, 3);
+		g.attachBoth(1, 4);
+		g.attachBoth(2, 4);
+		g.attachBoth(3, 4);
+		g.attachBoth(2, 4);
+		g.attachBoth(5, 6);
+		g.attachBoth(6, 7);
+		g.attachBoth(5, 7);
+		assertFalse(test.check(g));	
+	}
+
+	@Test
+	public void testCompleteWithSinglePoint() {
+		g.put(1, "a");
+		assertTrue(test.check(g));
+	}
+
+	@Test
+	public void testCompleteWithTwoPoints() {
+		g.put(1, "a");
+		g.put(2, "b");
+		assertFalse(test.check(g));
+	}
+
+	@Test
+	public void testCompleteWithAlmostCompleteGraph() {
+		g.put(1, "a");
+		g.put(2, "b");
+		g.put(3, "c");
+		g.put(4, "d");
+		g.attachBoth(1, 2);
+		g.attachBoth(1, 3);
+		g.attachBoth(1, 4);
+		g.attachBoth(3, 4);
+		g.attachBoth(2, 3);
+		assertFalse(test.check(g));
+	}
+
+	@Test
+	public void testCompleteWithCompleteGraphAndSinglePoint() {
+		g.put(1, "a");
+		g.put(2, "b");
+		g.put(3, "c");
+		g.put(4, "d");
+		g.put(5, "e");
+		g.attachBoth(1, 2);
+		g.attachBoth(1, 3);
+		g.attachBoth(1, 4);
+		g.attachBoth(3, 4);
+		g.attachBoth(2, 3);
+		assertFalse(test.check(g));
+	}
+}
